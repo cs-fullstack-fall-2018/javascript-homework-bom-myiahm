ee9782b7591ed2f02a66c3e779ceb6e898792b5a
Myiah Moore <myiahsade@gmail.com>
2018-08-14 21:01:57 -0500

    check in

---
+++ jss.js
@@ -0,0 +1,10 @@
+var   monitorInfo =
+    {
+        screeWidth:screen.width,
+        screenHeight:screen.height,
+        screenAvailableH:screen.availHeight,
+        screenAvailableW:screen.availWidth,
+        screenColor:screen.colorDepth,
+        screenPix: screen.pixelDepth
+    };
+document.write(monitorInfo);
